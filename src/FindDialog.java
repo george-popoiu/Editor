8688639431ea022cc885f1e90872974b293bcaba
george-popoiu <georgepopoiu@yahoo.com>
2012-08-19 12:07:02 +0300

    Added searching functionality using regex

--- src/FindDialog.java
+++ src/FindDialog.java
@@ -2,26 +2,36 @@ import java.awt.GridLayout;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.math.MathContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 
-public class FindDialog extends JDialog implements ActionListener{	
+public class FindDialog extends JDialog implements ActionListener, KeyListener{	
 	
 	Editor parent;
 	JLabel label;
 	JTextField textField;
 	JCheckBox caseSensitive;
 	JButton find, close;
+	boolean finishedFinding = true;
+	Matcher matcher;
 	
 	public FindDialog(Editor parent, boolean modal) {
 		super(parent, modal);
 		this.parent = parent;
+		getContentPane().addKeyListener(this);
+		getContentPane().setFocusable(true);
 		initComponents();
 		setTitle("Find");
 		setLocationRelativeTo(parent);
@@ -54,18 +64,70 @@ public class FindDialog extends JDialog implements ActionListener{
 		panel3.add(find);
 		panel3.add(close);
 		add(panel3);
+		textField.addKeyListener(this);
+		find.addKeyListener(this);
+		close.addKeyListener(this);
+		caseSensitive.addKeyListener(this);
+	}
+	
+	private void find(String pattern) {
+		if(!finishedFinding) {
+			if(matcher.find()) {
+				int selectionStart = matcher.start();
+				int selectionEnd = matcher.end();
+				parent.textPane.moveCaretPosition(matcher.start());
+				parent.textPane.select(selectionStart, selectionEnd);							
+			}
+			else {
+				finishedFinding = true;
+				JOptionPane.showMessageDialog(this, "You have reached the end of the file", 
+						"End of file", JOptionPane.INFORMATION_MESSAGE);
+				//closeDialog();
+			}
+		}
+		else {
+			matcher = Pattern.compile(pattern).matcher(parent.textPane.getText());
+			finishedFinding = false;
+			find(pattern);
+		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String cmd = e.getActionCommand();
 		if(cmd.equals("Find")) {
-			
+			String input = textField.getText();
+			StringBuilder pattern = new StringBuilder();
+			if( !caseSensitive.isSelected() ) {
+				pattern.append("(?i)");
+			}
+			pattern.append(input);
+			find(pattern.toString());
 		}
 		else if(cmd.equals("Close")) {
-			setVisible(false);
-			dispose();
+			closeDialog();
 		}
+	}
+	
+	private void closeDialog() {
+		setVisible(false);
+		dispose();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//System.out.println(e.getKeyCode());
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			closeDialog();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 	}
 	
 }
