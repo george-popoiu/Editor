87ca311330ced64ca3059603793c622a8fd22c4e
Archie Gunasekara <achintha@me.com>
2015-09-04 11:47:29 +1000

    File and Edit Tool bars were added New Help Menu, New Help->About Menu Item

--- src/editor/Editor.java
+++ src/editor/Editor.java
@@ -1,6 +1,7 @@
 package editor;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
@@ -12,6 +13,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -19,7 +21,9 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -32,10 +36,32 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 	}
 	
 	public JEditorPane textPane;
-	private JMenuBar menu;
+	private final JMenuBar menu;
+        private final JPanel toolbars;
+        private final JToolBar fileToolbar;
+        private final JToolBar editToolbar;
 	private JMenuItem copy, paste, cut;
 	public boolean changed = false;
-	private File file;	
+	private File file;
+        
+        private String applicationTitle = "Basic Java Text Editor";
+        
+        //setup icons - File Menu
+        private final ImageIcon newIcon = new ImageIcon("icons/new.png");
+        private final ImageIcon openIcon = new ImageIcon("icons/open.png");
+        private final ImageIcon saveIcon = new ImageIcon("icons/save.png");
+        private final ImageIcon saveAsIcon = new ImageIcon("icons/save_as.png");
+        private final ImageIcon quitIcon = new ImageIcon("icons/quit.png");
+        
+        //setup icons - Edit Menu
+        private final ImageIcon cutIcon = new ImageIcon("icons/cut.png");
+        private final ImageIcon copyIcon = new ImageIcon("icons/copy.png");
+        private final ImageIcon pasteIcon = new ImageIcon("icons/paste.png");
+        private final ImageIcon findIcon = new ImageIcon("icons/find.png");
+        private final ImageIcon selectAllIcon = new ImageIcon("icons/select_all.png");
+        
+        //setup icons - Helo Menu
+        private final ImageIcon aboutIcon = new ImageIcon("icons/about.png");
 	
 	public Editor() {
 		super("Editor");
@@ -45,8 +71,20 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 		
 		menu = new JMenuBar();
 		setJMenuBar(menu);
-		buildMenu();		
+		buildMenu();
+                
+                fileToolbar = new JToolBar();
+                buildFileToolBar();
+                
+                editToolbar = new JToolBar();
+                buildEditToolBar();
 		
+                //add both toolbars to a pannel
+                toolbars = new JPanel( new GridLayout(0, 1) );
+                toolbars.add(fileToolbar);
+                toolbars.add(editToolbar);
+                add(toolbars, BorderLayout.NORTH);
+                
 		setSize(500, 500);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,17 +93,11 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 	private void buildMenu() {		
 		buildFileMenu();
 		buildEditMenu();
+                buildHelpMenu();
 	}
 	
 	private void buildFileMenu() {
-            
-                //create icons
-                ImageIcon newIcon = new ImageIcon("icons/new.png");
-                ImageIcon openIcon = new ImageIcon("icons/open.png");
-                ImageIcon saveIcon = new ImageIcon("icons/save.png");
-                ImageIcon saveAsIcon = new ImageIcon("icons/save_as.png");
-                ImageIcon exitIcon = new ImageIcon("icons/exit.png");
-                
+       
 		JMenu file = new JMenu("File");
 		file.setMnemonic('F');
 		menu.add(file);
@@ -88,7 +120,7 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 		saveas.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK | InputEvent.SHIFT_MASK));
 		file.add(saveas);
 		saveas.addActionListener(this);		
-		JMenuItem quit = new JMenuItem("Quit", exitIcon);
+		JMenuItem quit = new JMenuItem("Quit", quitIcon);
 		file.add(quit);
 		quit.addActionListener(this);
 		quit.setMnemonic('Q');
@@ -96,13 +128,6 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 	}
 	
 	private void buildEditMenu() {
-            
-                //create icons
-                ImageIcon cutIcon = new ImageIcon("icons/cut.png");
-                ImageIcon copyIcon = new ImageIcon("icons/copy.png");
-                ImageIcon pasteIcon = new ImageIcon("icons/paste.png");
-                ImageIcon findIcon = new ImageIcon("icons/find.png");
-                ImageIcon selectAllIcon = new ImageIcon("icons/select_all.png");
 
 		JMenu edit = new JMenu("Edit");
 		menu.add(edit);
@@ -138,6 +163,94 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 		sall.addActionListener(this);
 		edit.add(sall);
 	}
+        
+        private void buildHelpMenu() {
+            
+            JMenu help = new JMenu("Help");
+            menu.add(help);
+            help.setMnemonic('h');		
+
+            //cut
+            JMenuItem about = new JMenuItem("About", aboutIcon);
+            about.addActionListener(this);
+            about.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
+            about.setMnemonic('A');
+            help.add(about);
+        }
+        
+        private void buildFileToolBar() {
+                
+                JButton newButton = new JButton(newIcon);
+                newButton.setActionCommand("New");
+                newButton.setToolTipText("New");
+                newButton.addActionListener(this);
+                fileToolbar.add(newButton);
+                fileToolbar.addSeparator();
+                
+                JButton openButton = new JButton(openIcon);
+                openButton.setActionCommand("Open");
+                openButton.setToolTipText("Open");
+                openButton.addActionListener(this);
+                fileToolbar.add(openButton);
+                fileToolbar.addSeparator();
+                
+                JButton saveButton = new JButton(saveIcon);
+                saveButton.setActionCommand("Save");
+                saveButton.setToolTipText("Save");
+                saveButton.addActionListener(this);
+                fileToolbar.add(saveButton);
+                fileToolbar.addSeparator();
+                
+                JButton saveAsButton = new JButton(saveAsIcon);
+                saveAsButton.setActionCommand("Save as...");
+                saveAsButton.setToolTipText("Save as...");
+                saveAsButton.addActionListener(this);
+                fileToolbar.add(saveAsButton);
+                fileToolbar.addSeparator();
+                
+                JButton quitButton = new JButton(quitIcon);
+                quitButton.setActionCommand("Quit");
+                quitButton.setToolTipText("Quit");
+                quitButton.addActionListener(this);
+                fileToolbar.add(quitButton);
+        }
+        
+        private void buildEditToolBar() {
+
+                JButton cutButton = new JButton(cutIcon);
+                cutButton.setActionCommand("Cut");
+                cutButton.setToolTipText("Cut");
+                cutButton.addActionListener(this);
+                editToolbar.add(cutButton);
+                editToolbar.addSeparator();
+                
+                JButton copyButton = new JButton(copyIcon);
+                copyButton.setActionCommand("Copy");
+                copyButton.setToolTipText("Copy");
+                copyButton.addActionListener(this);
+                editToolbar.add(copyButton);
+                editToolbar.addSeparator();
+                
+                JButton pasteButton = new JButton(pasteIcon);
+                pasteButton.setActionCommand("Paste");
+                pasteButton.setToolTipText("Paste");
+                pasteButton.addActionListener(this);
+                editToolbar.add(pasteButton);
+                editToolbar.addSeparator();
+                
+                JButton findButton = new JButton(findIcon);
+                findButton.setActionCommand("Find");
+                findButton.setToolTipText("Find");
+                findButton.addActionListener(this);
+                editToolbar.add(findButton);
+                editToolbar.addSeparator();
+                
+                JButton selectAllButton = new JButton(selectAllIcon);
+                selectAllButton.setActionCommand("Select All");
+                selectAllButton.setToolTipText("Select All");
+                selectAllButton.addActionListener(this);
+                editToolbar.add(selectAllButton);
+        }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -172,8 +285,21 @@ public class Editor extends JFrame implements ActionListener, DocumentListener {
 		else if(action.equals("Find")) {
 			FindDialog find = new FindDialog(this, true);
 			find.showDialog();
-		}		
+		}
+                else if(action.equals("About")) {
+			JOptionPane.showMessageDialog(this, getAboutText(), applicationTitle + " - About", JOptionPane.INFORMATION_MESSAGE, aboutIcon);
+                }
 	}
+        
+        private String getAboutText() {
+                String aboutText; 
+                aboutText = "Base Source From: https://github.com/george-popoiu/Editor\n";
+                aboutText += "Improved by Achintha Gunasekara : https://github.com/achinthagunasekara/Editor\n";
+                aboutText += "\n\nAchintha Gunasekara\n";
+                aboutText += "contact@achinthagunasekara.com\n";
+                aboutText += "http://www.achinthagunasekara.com";
+                return aboutText;
+        }
 	
 	private void newFile() {
 		if(changed) saveFile();		
